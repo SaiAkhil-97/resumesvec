62da44ac62289742d80508852bf574a27517744f
SaiAkhil-97 <prakash.saiakhil@gmail.com>
2018-04-03 16:33:30 +0530

    main.js

--- main.js
+++ main.js
@@ -12,4 +12,28 @@ function loadJSON(file,callback){
 loadJSON("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
+  carreer(data.carreer)
 })
+var right=document.querySelector(".content");
+function carreer(car){
+  console.log(car.info);
+  var h3=document.createElement("h3");
+  h3.textContent="carreer objective";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var p=document.createElement("p");
+  p.textContent=car.info;
+  right.appendChild(p);
+}
+function education(edu){
+  var h3=document.createElement("h3");
+  h3.textContent="Education";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var p=document.createElement("p");
+  p.textContent=car.info;
+  right.appendChild(p);
+
+}
