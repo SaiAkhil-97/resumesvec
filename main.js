046df1285240eee252d05e028a33118fb9692867
SaiAkhil-97 <prakash.saiakhil@gmail.com>
2018-04-04 12:36:33 +0530

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function loadJSON(file,callback){
   var xhr=new XMLHttpRequest();
   xhr.overrideMimeType("application/json");
@@ -12,7 +13,9 @@ function loadJSON(file,callback){
 loadJSON("data.json",function(text){
   let data=JSON.parse(text);
   console.log(data);
-  carreer(data.carreer)
+  carreer(data.carreer);
+  education(data.education);
+  skills(data.skills);
 })
 var right=document.querySelector(".content");
 function carreer(car){
@@ -32,8 +35,41 @@ function education(edu){
   right.appendChild(h3);
   var hr=document.createElement("hr");
   right.appendChild(hr);
-  var p=document.createElement("p");
-  p.textContent=car.info;
-  right.appendChild(p);
+  var ul=document.createElement("ul");
+  right.appendChild(ul);
 
+  for(var i=edu.length-1;i>=0;i--){
+    console.log(edu[i].degree);
+    var li=document.createElement("li");
+    li.textContent=edu[i].degree;
+    ul.appendChild(li);
+
+  var p=document.createElement("p");
+    p.textContent=edu[i].school;
+    li.appendChild(p);
+
+    var ul1=document.createElement("ul");
+    ul.appendChild(ul1);
+    for(var j=0;j<edu[i].data.length;j++){
+      var li1=document.createElement("li");
+      li1.textContent=edu[i].data[j];
+      ul1.appendChild(li1);
+
+    }
+  }
+}
+function skills(skill){
+  var h=document.createElement("h3");
+  h.textContent="skills";
+  right.appendChild(h);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var table=document.createElement("table");
+  table.border="1";
+  right.appendChild(table);
+  let row="";
+  for(var i=0;i<skill.length;i++){
+    row+="<tr><td>"+skill[i].name+"</td><td>"+skill[i].info+"</td></tr>"
+  }
+  table.innerHTML=row;
 }
